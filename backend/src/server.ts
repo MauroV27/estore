d90281cbe1fd75c1a8413d582a93601bc6917981
danielptj <84473076+danielptj@users.noreply.github.com>
2022-11-13 18:53:30 -0300

    WIP front-back integration with axios

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,19 @@ import express from "express";
 import { router } from "./routes/routes";
 
 const server = express();
+const cors = require("cors");
+var corsParam = {origin: "http://localhost:3001", optionsSuccessStatus: 200}
 
 server.use(express.json());
 server.use(router);
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  server.use(cors())
+  //res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+});
 
 const port = 3000;
 
--- frontend/estore-front/src/App.js
+++ frontend/estore-front/src/App.js
@@ -2,6 +2,21 @@ import React from "react";
 import RoutesApp from "./routes";
 import { AuthProvider } from "./contexts/auth";
 import GlobalStyle from "./styles/global";
+import axios from "axios";
+
+const api = axios.create({baseURL: "http://localhost:3000", headers: {'Content-Type': 'application/json',}});
+//, timeout: 1000, headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
+
+const getUsuario = (userId) => {
+  api.get(`/user/${userId}`, {headers : {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json', 'X-Requested-With': 'XMLHttpRequest'}}).then((response) => {
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+};
+
+getUsuario(1)
 
 const App = () => (
   <AuthProvider>
@@ -9,5 +24,6 @@ const App = () => (
     <GlobalStyle />
   </AuthProvider>
 );
+
 
 export default App;
