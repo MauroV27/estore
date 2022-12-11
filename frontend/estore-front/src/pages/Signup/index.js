567a5f4948c17cd685fca275694bbfadeb5cb003
Mauro Victor <maurovictor13@gmail.com>
2022-12-11 20:16:23 -0300

    Create page for register user.

--- frontend/estore-front/src/pages/Signup/index.js
+++ frontend/estore-front/src/pages/Signup/index.js
@@ -5,68 +5,88 @@ import * as C from "./styles";
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
+import { createUser } from "../../actions/users";
+
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [emailConf, setEmailConf] = useState("");
-  const [senha, setSenha] = useState("");
-  const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const { signup } = useAuth();
+    // (name, email, password, adress, login)
 
-  const handleSignup = () => {
-    if (!email | !emailConf | !senha) {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [login, setLogin] = useState("");
+  const [error, setError] = useState("");
+  const [endereco, setEndereco] = useState("");
+
+  const handleRegister = async () => {
+    if ( !email | !senha | !nome | !login ) {
       setError("Preencha todos os campos");
       return;
-    } else if (email !== emailConf) {
-      setError("Os e-mails não são iguais");
-      return;
     }
 
-    const res = signup(email, senha);
+    const res = await createUser(nome, email, senha, endereco, login);
 
     if (res) {
-      setError(res);
-      return;
+        if ( res.data ) { 
+            const {message, status} = res.data;
+        
+            if ( status === "success" ){
+                navigate("/singin");
+                return;
+            } else {
+                setError(`Falha no cadastro. ${message}.`);
+            }
+        }
     }
 
-    alert("Usuário cadatrado com sucesso!");
-    navigate("/");
+    navigate("/singup")
   };
 
   return (
     <C.Container>
-      <C.Label>CADASTRO ESTORE</C.Label>
+      <C.Label>Criar conta</C.Label>
       <C.Content>
+      <Input
+          type="text"
+          placeholder="Digite seu nome :"
+          value={nome}
+          onChange={(e) => [setNome(e.target.value), setError("")]}
+        />
         <Input
-          type="email"
-          placeholder="Digite seu E-mail"
-          value={email}
-          onChange={(e) => [setEmail(e.target.value), setError("")]}
+          type="text"
+          placeholder="Digite seu login :"
+          value={login}
+          onChange={(e) => [setLogin(e.target.value), setError("")]}
         />
         <Input
           type="email"
-          placeholder="Confirme seu E-mail"
-          value={emailConf}
-          onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+          placeholder="Digite seu E-mail :"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />        
+        <Input
+          type="text"
+          placeholder="Digite seu endereço :"
+          value={endereco}
+          onChange={(e) => [setEndereco(e.target.value), setError("")]}
         />
         <Input
           type="password"
-          placeholder="Digite sua Senha"
+          placeholder="Digite sua Senha :"
           value={senha}
           onChange={(e) => [setSenha(e.target.value), setError("")]}
         />
         <C.labelError>{error}</C.labelError>
-        <Button Text="Inscrever-se" onClick={handleSignup} />
+        <Button Text="Registrar" onClick={handleRegister} />
         <C.LabelSignin>
           Já tem uma conta?
           <C.Strong>
-            <Link to="/">&nbsp;Entre</Link>
+            <Link to="/singin">&nbsp;Login</Link>
           </C.Strong>
         </C.LabelSignin>
       </C.Content>
     </C.Container>
   );
 };
-
 export default Signup;
